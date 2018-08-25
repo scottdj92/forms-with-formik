654374fb014417d1f9342bf35eb6d37184349fe6
Scott Jones <scottdj92@gmail.com>
2018-08-25 15:31:55 -0400

    initial-form: add basic form

--- app.tsx
+++ app.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { Formik } from "formik";
 
+const initialValues = {
+    firstName: "",
+    pet: "",
+};
+
 const App: React.SFC = () => (
     <>
-        <h1>hello world</h1>
+        <h1>Working with Formik</h1>
+        <Formik initialValues={initialValues}
+            onSubmit={(values) => console.log(values)}
+            render={({ handleSubmit, handleChange }) => (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="firstName">
+                        <div>First Name</div>
+                        <input type="text" name="firstName"/>
+                    </label>
+                    <label htmlFor="pet">
+                        <div>Pet</div>
+                        <select name="pet" onChange={handleChange}>
+                            <option>Dog</option>
+                            <option>Cat</option>
+                            <option>Other</option>
+                        </select>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            )}>
+        </Formik>
     </>
 );
 
