b6fca6e14583d40fd77e6e35b9ea92dd023dba30
Scott Jones <scottdj92@gmail.com>
2018-08-26 09:54:54 -0400

    case/styled-fields: add styles

--- TextField.tsx
+++ TextField.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { FieldProps } from "formik";
 import { FormValues } from "./app";
+import styled from "styled-components";
+
+const Title = styled.h2`
+    font-family: Arial, Helvetica, sans-serif;
+    color: cornflowerblue;
+    margin-bottom: 5px;
+`;
+
+const FormInput = styled.input`
+    border-color: gray;
+    border: 1px solid;
+    padding: 5px 5px;
+    margin-bottom: 5px;
+`;
+
+const InputError = styled.div`
+    color: red;
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 15px 0;
+`;
 
 interface Props {
     title: string;
@@ -9,11 +29,11 @@ interface Props {
 type OwnProps = FieldProps<FormValues> & Props;
 const TextField: React.SFC<OwnProps> = ({ title, field, form }) => (
     <label htmlFor={field.name}>
-        <div>{title}</div>
-        <input type="text" {...field}/>
+        <Title>{title}</Title>
+        <FormInput type="text" {...field}/>
         {
             form.touched[field.name] && form.errors[field.name]
-            ? <div>{form.errors[field.name]}</div>
+            ? <InputError>{form.errors[field.name]}</InputError>
             : null
         }
     </label>
